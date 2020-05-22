d632ef19cdf5b17a6bf1652eab1d246f30520db1
moyanDev <109855524+moyanDev@users.noreply.github.com>
2020-05-22 16:03:03 +0000

    Travis config

--- bollinger_bands.go
+++ bollinger_bands.go
@@ -7,4 +7,4 @@ package tago
 
 /*
 BollingerBands
-The Bollinger Bands are represented by Average EMA and standard deviaton that is moved 'k' times away i
+The Bollinger Bands are represented by Average EMA and standard deviaton that is moved 'k' times away in both directions from 
