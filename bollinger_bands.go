43b104c7dc2c563e3ad85364089ad538ad11637e
moyanDev <109855524+moyanDev@users.noreply.github.com>
2020-05-25 16:16:21 +0000

    Prep for 2.5

--- bollinger_bands.go
+++ bollinger_bands.go
@@ -13,4 +13,4 @@ The Bollinger Bands are represented by Average EMA and standard deviaton that is
 
 See SMA, SD documentation.
 
-* _BB<sub>Middle Band</sub>_ - Si
+* _BB<sub>Middle Band</sub>_ - Simple Moving Aver
