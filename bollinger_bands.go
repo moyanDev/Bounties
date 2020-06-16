730327a4258cdcd2a194cfc40967b538445f3462
moyanDev <109855524+moyanDev@users.noreply.github.com>
2020-06-16 20:57:54 +0000

    A few minor changes

--- bollinger_bands.go
+++ bollinger_bands.go
@@ -38,4 +38,7 @@ type BollingerBands struct {
 // // Example: NewBollingerBands(9)
 // func NewBollingerBands(n, multiplier int) (*BollingerBands, error) {
 // 	if n <= 0 || multiplier <= 0 {
-// 		return nil, ErrInvalidPa
+// 		return nil, ErrInvalidParameters
+// 	}
+
+// 	sd, er
