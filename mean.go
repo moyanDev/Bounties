40fd6e27ddc23594fc3db67da1c5a328e08b28d7
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-01-30 20:15:36 +0000

    Asoc:msm:Added Buffer overflow check

--- mean.go
+++ mean.go
@@ -14,4 +14,8 @@ Mean returns the arithmetic mean value of the last n values.
 
 # Formula
 
-![SD formula](https://wikimedia.org/api/rest_v1/media/math/render/svg/97821b8c43e3182faa22db06932846d
+![SD formula](https://wikimedia.org/api/rest_v1/media/math/render/svg/97821b8c43e3182faa22db06932846d1550866fb_
+
+Where:
+
+* _n_ - numb
