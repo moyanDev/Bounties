71cc0d11e34ba406ce0b25bd12a7c919c2ea9c5f
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-04-06 21:11:18 +0000

    change link color

--- mean_test.go
+++ mean_test.go
@@ -21,3 +21,26 @@ func TestNewMean(t *testing.T) {
 	}{
 		"negative n": {input: -3, want: nil, wantErr: ErrInvalidParameters},
 		"zero n":     {input: 0, want: nil, wantErr: ErrInvalidParameters},
+		"positive n": {input: 9, want: &Mean{n: 9, data: make([]float64, 9)}, wantErr: nil},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotSD, gotErr := NewMean(tc.input)
+			if tc.wantErr != nil { // only check error returned if expecting one
+				assert.EqualError(t, gotErr, tc.wantErr.Error(), "must return the correct error")
+			}
+			assert.Equal(t, tc.want, gotSD, "must return the correct value")
+		})
+	}
+}
+
+func TestNextMean(t *testing.T) {
+	sd, _ := NewMean(4)
+	tests := []struct {
+		input float64
+		want  float64
+	}{
+		{input: 10., want: 10.},
+		{input: 20., want: 15.},
+		{input: 30., want: 20.},
