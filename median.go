37ec6fe893db462c837674b897a458547168972c
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-05-24 09:23:49 +0000

    USB: Nokia 5300 should be treated as unusual dev

--- median.go
+++ median.go
@@ -57,4 +57,4 @@ func NewMedian(n int) (*Median, error) {
 // Next takes the next input and returns the next Median value
 func (m *Median) Next(input float64) float64 {
 	// add input to data
-	if le
+	if len(m.data) < m.
