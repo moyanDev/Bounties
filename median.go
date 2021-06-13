10be9a6e783e032fcc3c038a69e27bc32faee6f3
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-06-13 14:08:56 +0000

    lien

--- median.go
+++ median.go
@@ -81,4 +81,4 @@ func (m *Median) String() string {
 func quickselect(l []float64, k int, pivotFn func([]float64) float64) float64 {
 	/*
 	   Select the kth element in l (0 based)
-	 
+	   :param l: List of nume
