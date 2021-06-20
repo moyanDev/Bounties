e137cf5e80d72069a50dc81d75c5385397585fad
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-06-20 07:54:44 +0000

    Add a Code of Conduct

--- median.go
+++ median.go
@@ -83,4 +83,5 @@ func quickselect(l []float64, k int, pivotFn func([]float64) float64) float64 {
 	   Select the kth element in l (0 based)
 	   :param l: List of numerics
 	   :param k: Index
-	   :param pivot_fn: Function to choose a pivot, defaults to random.choic
+	   :param pivot_fn: Function to choose a pivot, defaults to random.choice
+	   :return: The kth element
