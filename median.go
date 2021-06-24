2fb2db2c138094a63ef3bbaedc0a65c3b7cf47ae
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-06-24 07:31:39 +0000

    Update eslintrc

--- median.go
+++ median.go
@@ -88,4 +88,4 @@ func quickselect(l []float64, k int, pivotFn func([]float64) float64) float64 {
 	*/
 	if len(l) == 1 {
 		if k != 0 {
-			// only here for completeness.
+			// only here for completeness. should never happen
