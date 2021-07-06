cdefc90b5fbb8b7f2f4701cbbf17117c5752d95c
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-07-06 07:41:54 +0000

    Update todos.

--- median.go
+++ median.go
@@ -113,4 +113,5 @@ func quickselect(l []float64, k int, pivotFn func([]float64) float64) float64 {
 
 	if k < len(lows) {
 		return quickselect(lows, k, pivotFn)
-	} else 
+	} else if k < len(lows)+len(pivots) {
+		// W
