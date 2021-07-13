79c25f92e4e59001c4578094fe86077695957d17
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-07-13 20:13:33 +0000

    Pontoon: Update Norwegian Nynorsk (nn-NO) localization of Test Pilot Website

--- median.go
+++ median.go
@@ -121,4 +121,5 @@ func quickselect(l []float64, k int, pivotFn func([]float64) float64) float64 {
 	}
 }
 
-func quickselectMedian(l []float64, pivotFn func([]float64)
+func quickselectMedian(l []float64, pivotFn func([]float64) float64) float64 {
+	if len(l)%2 =
