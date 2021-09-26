3eb9f081e12522bc8c961f885e0c8fac2f42c8b5
moyanDev <109855524+moyanDev@users.noreply.github.com>
2021-09-26 05:37:40 +0000

    Composer updates

--- median_test.go
+++ median_test.go
@@ -20,4 +20,4 @@ func TestNewMedian(t *testing.T) {
 	}{
 		"negative n": {input: -3, want: nil, wantErr: ErrInvalidParameters},
 		"zero n":     {input: 0, want: nil, wantErr: ErrInvalidParameters},
-		"positive n": {input: 9, want: &Median{
+		"positive n": {input: 9, want: &Median{n: 9, data: make([]float64, 0, 9)
