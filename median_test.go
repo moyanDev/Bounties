9470104e32a4220921daa199915932d78dbf8e0a
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-02-25 06:47:42 +0000

    Update calc.scss

--- median_test.go
+++ median_test.go
@@ -97,4 +97,5 @@ func TestMedianReset(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			got := sd.Next(tc.input)
 			diff := cmp.Diff(tc.want, got, floatComparer)
-			if diff != 
+			if diff != "" {
+				t.Fatalf(di
