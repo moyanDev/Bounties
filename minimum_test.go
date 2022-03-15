65c3485110a2872b7ac3e42f24a6c89991d4e8e6
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-03-15 19:23:34 +0000

    organisation

--- minimum_test.go
+++ minimum_test.go
@@ -19,3 +19,30 @@ func TestNewMinimum(t *testing.T) {
 	for i := range data {
 		data[i] = math.Inf(1)
 	}
+
+	tests := map[string]struct {
+		input   int
+		want    *Minimum
+		wantErr error
+	}{
+		"negative n": {input: -3, want: nil, wantErr: ErrInvalidParameters},
+		"zero n":     {input: 0, want: nil, wantErr: ErrInvalidParameters},
+		"positive n": {input: 9, want: &Minimum{n: 9, data: data}, wantErr: nil},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotSD, gotErr := NewMinimum(tc.input)
+			if tc.wantErr != nil { // only check error returned if expecting one
+				assert.EqualError(t, gotErr, tc.wantErr.Error(), "must return the correct error")
+			}
+			assert.Equal(t, tc.want, gotSD, "must return the correct value")
+		})
+	}
+}
+
+func TestMinimumNext(t *testing.T) {
+	sd, _ := NewMinimum(3)
+	tests := []struct {
+		input float64
+		want  float64
