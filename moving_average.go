c39e2586b7b1dc2b778fbb0576e900d98c408948
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-04-07 19:24:35 +0000

    Fix highlighting

--- moving_average.go
+++ moving_average.go
@@ -12,4 +12,6 @@ MovingAverage returns the average in _n_ number of periods
 
 # Formula
 
-![SMA](https://wikimedia.org/api/rest_v1/media/math/render/svg/e2bf09dc6deaf86b36070
+![SMA](https://wikimedia.org/api/rest_v1/media/math/render/svg/e2bf09dc6deaf86b3607040585fac6078f9c7c89)
+
+Where
