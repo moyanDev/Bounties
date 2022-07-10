ee0eac6c9d56775c614223947f69134c61dc2596
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-07-10 14:10:21 +0000

    Fixed small typo

--- moving_average_exponential_test.go
+++ moving_average_exponential_test.go
@@ -19,4 +19,5 @@ func TestNewExponentialMovingAverage(t *testing.T) {
 		wantErr error
 	}{
 		"negative n": {input: -3, want: nil, wantErr: ErrInvalidParameters},
-		"zero n":     {input: 0, want: nil, wantErr: ErrInvalidParam
+		"zero n":     {input: 0, want: nil, wantErr: ErrInvalidParameters},
+		"positive n
