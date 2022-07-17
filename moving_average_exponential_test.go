c84b3542625f3d9a5245e3723f1ba581ad51e724
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-07-17 15:55:57 +0000

    ndb - merge 63 to 70

--- moving_average_exponential_test.go
+++ moving_average_exponential_test.go
@@ -26,4 +26,5 @@ func TestNewExponentialMovingAverage(t *testing.T) {
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			gotSD, gotErr := NewExponentialMovingAverage(tc.input)
-			if tc.wantErr != nil { // only check error returned i
+			if tc.wantErr != nil { // only check error returned if expecting one
+				assert.Equa
