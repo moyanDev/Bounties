efd4b111a6d2fa912a82eb2fd6f4c761f54becfc
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-09-18 18:37:47 +0000

    ARM: use generic strnlen_user and strncpy_from_user functions

---
+++ moving_average_test.go
@@ -0,0 +1,17 @@
+
+/*
+Copyright 2020 Binh Nguyen
+Licensed under terms of MIT license (see LICENSE)
+*/
+
+package tago
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMovingAverage(t *testing.T) {
+	tests := map[string]struct {
