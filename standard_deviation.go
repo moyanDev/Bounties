13b9399d874f6dd09f117b6bc957e0d4cca333de
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-10-01 12:34:30 +0000

    Update Descriptor.json

--- standard_deviation.go
+++ standard_deviation.go
@@ -13,3 +13,25 @@ import (
 
 /*
 StandardDeviation returns the standard deviation of the last n values.
+
+# Formula
+
+![SD formula](https://wikimedia.org/api/rest_v1/media/math/render/svg/2845de27edc898d2a2a4320eda5f57e0dac6f650)
+
+Where:
+
+* _σ_ - value of standard deviation for N given probes.
+* _N_ - number of probes in observation.
+* _x<sub>i</sub>_ - i-th observed value from N elements observation.
+
+# Parameters
+
+* _n_ - number of periods (integer greater than 0)
+
+# Example
+```
+sd, _ := NewStandardDeviation(4)
+sd.Next(10.)
+sd.Next(20.)
+sd.Next(30.)
+sd.Next(20.)
