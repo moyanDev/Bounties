4b76d3e19040f8ccf4d661743adedc145d93f11c
moyanDev <109855524+moyanDev@users.noreply.github.com>
2022-10-22 22:06:03 +0000

    Update VERSION.txt

--- standard_deviation_test.go
+++ standard_deviation_test.go
@@ -19,4 +19,4 @@ func TestNewStandardDeviation(t *testing.T) {
 		wantErr error
 	}{
 		"negative n": {input: -3, want: nil, wantErr: ErrInvalidParameters},
-		"zero n":     
+		"zero n":     {input: 0, want: nil, wantErr: E
